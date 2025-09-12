9967abe594957832c4b833b9a5db3444eb076323
Nicky Gerritsen <github@accounts.nickygerritsen.nl>
2025-09-12 14:09:28 +0200

    Don't set a default contest ID so auto detection works (#49)

--- src/lib/hardcoded.svelte.ts
+++ src/lib/hardcoded.svelte.ts
@@ -2,7 +2,7 @@ import { browser } from '$app/environment';
 
 export const CONTEST = $state(browser ? {} : {
     url: process?.env?.CONTEST_URL || 'https://localhost:8443/api/',
-    contest_id: process?.env?.CONTEST_ID || 'systest',
+    contest_id: process?.env?.CONTEST_ID || undefined,
     user: process?.env?.CONTEST_USER || 'admin',
     password: process?.env?.CONTEST_PASSWORD || 'adm1n',
 });
